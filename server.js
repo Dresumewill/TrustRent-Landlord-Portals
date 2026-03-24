b5d23069bfdd9d2e00de941cdc3b25a616ed595a
Ifeyinwa <ifeyinwaofu@gmail.com>
2026-03-24 23:16:40 +0100

    feat: initial commit

--- server.js
+++
@@ -1,9 +0,0 @@
-require(`dotenv`).config();
-const express = require(`express`);
-const multer = require(`multer`);
-const { pool } = require(`pg`);
-const cors = require(`cors`);
-const path = require(`path`);
-
-const app = express();
-const PORT = 3000;
